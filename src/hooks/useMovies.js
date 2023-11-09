7ec9aad330f1978a615ebdd9dffd2310e5724978
Islam Kamal <islamkamalx1>
2023-11-09 16:31:00 +0200

    edit the  end point

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -20,7 +20,7 @@ export function useMovies(query) {
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${apiKey}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${apiKey}&s=${query}`,
             { signal: controller.signal }
           );
 
