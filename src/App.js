1dec15f0b03440cf67d67e3ab4feffa580bbb65c
Islam Kamal <islamkamalx1>
2023-11-09 16:25:36 +0200

    uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useMovies } from "./hooks/useMovies";
+import { useLocalStorageState } from "./hooks/useLocalStorageState";
+import Loader from "./components/Loader";
+import ErrorMessage from "./components/ErrorMessage";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Logo from "./components/Logo";
+import SearchInput from "./components/SearchInput";
+import NumResult from "./components/NumResult";
+import Box from "./components/Box";
+import MoviesList from "./components/MoviesList";
+import MovieDetails from "./components/MovieDetails";
+import WatchedSummary from "./components/WatchedSummary";
+import WatchedMoviesList from "./components/WatchedMoviesList";
 
-function App() {
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
+
+  const { movies, isLoading, error } = useMovies(query);
+
+  const [watched, setWatched] = useLocalStorageState([], "Watched");
+
+  function handleMovieSelect(id) {
+    setSelectedId((selected) => (id === selected ? null : id));
+  }
+
+  function handleCloseMovie() {
+    setSelectedId(null);
+  }
+
+  function handleAddMovie(movie) {
+    setWatched((watched) => [...watched, movie]);
+  }
+
+  function handleDeleteWatched(id) {
+    setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar>
+        <Logo />
+        <SearchInput query={query} setQuery={setQuery} />
+        <NumResult movies={movies} />
+      </Navbar>
+      <Main>
+        <Box>
+          {isLoading && <Loader />}
+          {!isLoading && !error && (
+            <MoviesList movies={movies} onSelectMovie={handleMovieSelect} />
+          )}
+          {error && <ErrorMessage message={error} />}
+        </Box>
+        <Box>
+          {selectedId ? (
+            <MovieDetails
+              selectedId={selectedId}
+              onCloseMovie={handleCloseMovie}
+              onAddWatched={handleAddMovie}
+              watched={watched}
+            />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList
+                watched={watched}
+                onDeleteWatched={handleDeleteWatched}
+              />
+            </>
+          )}
+        </Box>
+      </Main>
+    </>
   );
 }
-
-export default App;
---
+++ src/components/MoviesList.js
@@ -0,0 +1,13 @@
+import Movie from "../components/Movie"
+
+const MoviesList = ({ movies, onSelectMovie }) => {
+  return (
+    <ul className="list list-movies">
+      {movies?.map((movie) => (
+        <Movie onSelectMovie={onSelectMovie} movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+};
+
+export default MoviesList;
